7586d0505fd3dee3d89e53104470453260920852
johanbin <whgksqls9999@naver.com>
2024-08-02 17:16:23 +0900

    내역 입력 기능 구현

--- frontend/public/js/inputform/handleSubmit.js
+++ frontend/public/js/inputform/handleSubmit.js
@@ -1,9 +1,9 @@
 export default async function handleSubmit() {
-  const category = handleCategory();
+  const classification = handleClassification();
   const transactionDate = handleDate();
   const asset = document.querySelector('#asset').value;
-  const classification = document.querySelector('#classification').value;
-  const amount = document.querySelector('#amount').value;
+  const category = document.querySelector('#category').value;
+  const amount = +document.querySelector('#amount').value;
   const description = document.querySelector('#description').value;
   const memo = document.querySelector('#memo').value;
 
@@ -27,15 +27,16 @@ export default async function handleSubmit() {
     },
     body: JSON.stringify(obj),
   });
-  console.log(obj);
+
+  console.log(result);
 }
 
 // 카테고리 체크
-function handleCategory() {
-  const categorys = document.getElementsByName('category');
+function handleClassification() {
+  const classifications = document.getElementsByName('classification');
   let result;
 
-  categorys.forEach((it) => {
+  classifications.forEach((it) => {
     if (it.checked) {
       result = it.value;
       return;
@@ -50,5 +51,5 @@ function handleDate() {
   const date = document.querySelector('#date');
   const time = document.querySelector('#time');
 
-  return new Date(`${date.value} ${time.value}`);
+  return `${date.value} ${time.value}:00`;
 }
