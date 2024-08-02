7ae0f8f22834c3b18990b99d3dd512253fab2a9c
chase62117 <134244895+chase62117@users.noreply.github.com>
2024-08-02 16:45:58 +0900

    cors 추가

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+let corsOptions = {
+  origin: 'https://localhost:3000/',
+  credentials: true
+}
+
+app.use(cors(corsOptions));
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
