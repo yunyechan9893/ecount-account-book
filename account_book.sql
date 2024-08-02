ee6efb51e3e0bf4487d5dd485aee555619778d8a
chase62117 <134244895+chase62117@users.noreply.github.com>
2024-08-02 17:32:11 +0900

    테이블 업데이트

--- account_book.sql
+++ account_book.sql
@@ -8,20 +8,32 @@ CREATE TABLE IF NOT EXISTS `account_book`.`member` (
   UNIQUE INDEX `member_id_UNIQUE` (`member_id` ASC) VISIBLE
 ) ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `account_book`.`finance` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `member_id` VARCHAR(255) NOT NULL,
-  `transaction_date` TIMESTAMP NOT NULL,
-  `category` VARCHAR(255) NOT NULL,
-  `description` VARCHAR(255) NOT NULL,
-  `amount` BIGINT NOT NULL,
-  `memo` TEXT NULL,
-  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  `modified_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+CREATE TABLE `category` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `category_type` varchar(255) NOT NULL,
+  `major_category` varchar(255) NOT NULL,
+  `middle_category` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+
+CREATE TABLE `finance` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `member_id` bigint DEFAULT NULL,
+  `transaction_date` timestamp NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `amount` bigint NOT NULL,
+  `memo` text,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `asset` varchar(255) DEFAULT NULL,
+  `classification` varchar(255) DEFAULT NULL,
+  `category_id` bigint DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `member_id_idx` (`member_id` ASC),
-  CONSTRAINT `fk_member`
-    FOREIGN KEY (`member_id`)
-    REFERENCES `account_book`.`member` (`member_id`)
-    ON DELETE NO ACTION
-) ENGINE = InnoDB;
+  KEY `member_id_idx` (`member_id`),
+  KEY `fk_finance_category` (`category_id`),
+  CONSTRAINT `fk_finance_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`),
+  CONSTRAINT `fk_finance_member_id` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
